e4cd6723006b6827d20ed2a67872f19416a40765
andygnewman <andrew.newman@ft.com>
2015-12-17 17:35:55 +0000

    remove primary brand determination logic from here

--- tests/main.test.js
+++ tests/main.test.js
@@ -6,7 +6,6 @@ const should = require('chai').should();
 const getBranding = require('../main');
 
 const brandMetadata = require('./fixtures/metadata/brandMetadata');
-const brandAndColumnistMetadata = require('./fixtures/metadata/brandAndColumnistMetadata');
 const columnistMetadata = require('./fixtures/metadata/columnistMetadata');
 const columnistHasHeadshotMetadata = require('./fixtures/metadata/columnistHasHeadshotMetadata');
 const defaultMetadata = require('./fixtures/metadata/defaultMetadata');
@@ -16,23 +15,19 @@ const columnist = require('./fixtures/branding/columnist');
 const columnistHeadshot = require('./fixtures/branding/columnistHeadshot');
 
 describe('Branding', function () {
-	it('should set branding to the article brand if present', function() {
+	it('should set branding to brand if it has primary brand present', function() {
 		getBranding(brandMetadata).should.eql(brand);
 	});
 
-	it('should set branding to the article columnist if present', function() {
+	it('should set branding to the author if it has primary brand present', function() {
 		getBranding(columnistMetadata).should.eql(columnist);
-	});
-
-	it('should set branding to the brand if both brand (not equal to author) and columnist are present', function() {
-		getBranding(brandAndColumnistMetadata).should.eql(brand);
 	});
 
 	it('should set a headshot to the branding if a columnist and has hasHeadshot attribute', function () {
 		getBranding(columnistHasHeadshotMetadata).should.eql(columnistHeadshot);
 	});
 
-	it('should return null if no brand or columnist is present', function() {
+	it('should return null if no primary brand is present', function() {
 		should.not.exist(getBranding(defaultMetadata));
 	});
 
